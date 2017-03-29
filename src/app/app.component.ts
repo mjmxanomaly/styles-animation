0697ea91d4a77b7f584b4dceeb18ee6782df3446
mjmxanomaly <mjmxanomaly@gmail.com>
2017-03-29 16:34:37 -0700

    animation module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+  	trigger('clickedState', [
+  		state('default', style({
+  			backgroundColor: 'orange',
+  			width: '100px',
+  			height: '100px'
+  		})),
+  		state('clicked', style({
+  			backgroundColor: 'blue',
+  			width: '300px',
+  			height: '50px'
+  		})),
+  		transition('default <=> clicked', animate('1s 500ms ease-in'))
+  	])
+  ]
 })
 export class AppComponent {
   divClicked = false;
   doAnimate = false;
+  clickInfo = 'default';
+
+  	onClickSimple() {
+  		this.clickInfo = 'clicked';
+  		setTimeout(() => {
+  			this.clickInfo = 'default';
+  		}, 3000);
+  	}
 }
