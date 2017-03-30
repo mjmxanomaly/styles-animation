281ccae390646b0eace1708a90cd5d5f3a9f4f10
mjmxanomaly <mjmxanomaly@gmail.com>
2017-03-30 13:37:51 -0700

    Separated animations into its own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,13 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { clickedStateTrigger, numberEnteredStateTrigger } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-  	trigger('clickedState', [
-  		state('default', style({
-  			backgroundColor: 'orange',
-  			width: '100px',
-  			height: '100px'
-  		})),
-  		state('clicked', style({
-  			backgroundColor: 'blue',
-  			width: '300px',
-  			height: '50px'
-  		})),
-  		state('mousedown', style({
-  			backgroundColor: 'red',
-  			width: '100px',
-  			height: '100px'
-  		})),
-  		transition('default <=> clicked', animate('1s 500ms ease-in')),
-  		transition('mousedown <=> clicked', animate(300)),
-  		transition('mousedown <=> default', animate(300))
-  	]),
-  	trigger('numberEnteredState', [
-  		state('unselected', style({
-  			border: '1px solid black',
-  			padding: '5px',
-  			backgroundColor: 'white'
-  		})),
-  		state('selected', style({
-  			border: '2px solid blue',
-  			padding: '4px',
-  			backgroundColor: 'lightblue'
-  		})),
-  		transition('unselected => selected', [
-  			style({
-  				border: '2px solid black',
-  				padding: '4px',
-  				transform: 'scale(1)'
-  			}),
-  			animate('600ms 100ms ease-out', style({
-  				backgroundColor: 'red',
-  				transform: 'scale(1.05)'
-  			})),
-  			animate(2000)	
-  		])
-  	])
+  	clickedStateTrigger,
+  	numberEnteredStateTrigger
   ]
 })
 export class AppComponent {
