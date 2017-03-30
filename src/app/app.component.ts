df9689b73208143580cf8843d744b8b5033954c3
mjmxanomaly <mjmxanomaly@gmail.com>
2017-03-30 13:32:52 -0700

    New animation with input value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,38 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   			width: '300px',
   			height: '50px'
   		})),
-  		transition('default <=> clicked', animate('1s 500ms ease-in'))
+  		state('mousedown', style({
+  			backgroundColor: 'red',
+  			width: '100px',
+  			height: '100px'
+  		})),
+  		transition('default <=> clicked', animate('1s 500ms ease-in')),
+  		transition('mousedown <=> clicked', animate(300)),
+  		transition('mousedown <=> default', animate(300))
+  	]),
+  	trigger('numberEnteredState', [
+  		state('unselected', style({
+  			border: '1px solid black',
+  			padding: '5px',
+  			backgroundColor: 'white'
+  		})),
+  		state('selected', style({
+  			border: '2px solid blue',
+  			padding: '4px',
+  			backgroundColor: 'lightblue'
+  		})),
+  		transition('unselected => selected', [
+  			style({
+  				border: '2px solid black',
+  				padding: '4px',
+  				transform: 'scale(1)'
+  			}),
+  			animate('600ms 100ms ease-out', style({
+  				backgroundColor: 'red',
+  				transform: 'scale(1.05)'
+  			})),
+  			animate(2000)	
+  		])
   	])
   ]
 })
@@ -25,6 +56,8 @@ export class AppComponent {
   divClicked = false;
   doAnimate = false;
   clickInfo = 'default';
+  paragraphClick = 'default';
+  numberEntered;
 
   	onClickSimple() {
   		this.clickInfo = 'clicked';
